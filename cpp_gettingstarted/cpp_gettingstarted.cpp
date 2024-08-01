c144ec7b74095b3ab747b17cf4a001b154e1832b
blossom ghuntla <blossom_ghuntla@yahoo.com>
2024-08-01 19:50:55 +0530

    escape sequences of different kinds

--- cpp_gettingstarted/cpp_gettingstarted.cpp
+++ cpp_gettingstarted/cpp_gettingstarted.cpp
@@ -1,18 +1,6 @@
-// Circumference and area of a circle with radius 2.5
 #include<iostream>
-//using namespace std;
-
-const double pi = { 3.141593 };
 
 int main() {
-	double area, circuit, radius = { 1.5 };
-
-	area = pi * radius * radius;
-	circuit = 2 * pi * radius;
-	std::cout << "\nTo Evaluate a Circle\n" << std::endl;
-	std::cout << "Radius:       " << radius << std::endl;
-	std::cout << "Circumference:       " << circuit << std::endl;
-	std::cout << "Area:       " << area << std::endl;
-
+	std::cout << "I \n\t\"RUSH\" \n\t\t\\TO\\ \n\tAND \n/FRO/" << std::endl;
 	return 0;
 }

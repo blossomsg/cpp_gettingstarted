f03850380985b4bf1c41ea2048dca6c5bec9303e
blossom ghuntla <blossom_ghuntla@yahoo.com>
2024-08-01 20:22:06 +0530

    sizeof operator exercise

--- cpp_gettingstarted/cpp_gettingstarted.cpp
+++ cpp_gettingstarted/cpp_gettingstarted.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 
 int main() {
-	std::cout << "I \n\t\"RUSH\" \n\t\t\\TO\\ \n\tAND \n/FRO/" << std::endl;
+	std::cout << sizeof(short) << std::endl;
+	std::cout << sizeof(long) << std::endl;
+	std::cout << sizeof(int) << std::endl;
+	std::cout << sizeof(bool) << std::endl;
+	std::cout << sizeof(char) << std::endl;
+	std::cout << sizeof(float) << std::endl;
+	std::cout << sizeof(double) << std::endl;
+	std::cout << sizeof(long double) << std::endl;
+
 	return 0;
 }

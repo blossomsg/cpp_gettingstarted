e641f3f4253df5cfffa8890b3f9991c12a6ea4a6
blossom ghuntla <blossom_ghuntla@yahoo.com>
2024-08-29 23:29:45 +0530

    get, string, iostream

--- cpp_gettingstarted/cpp_gettingstarted.cpp
+++ cpp_gettingstarted/cpp_gettingstarted.cpp
@@ -1,20 +1,15 @@
-#include<iostream>
-#include<iomanip>
-
-int main() {
-	unsigned char ch;
-	unsigned int num;
-
-	std::cin >> num;
-
-	ch = num;
-
-	std::cout << "The character:  " << ch << std::endl;
-
-	num = ch;
-	
-	std::cout << std::uppercase << std::hex << ch << std::endl;
-	std::cout << std::oct << "\n" <<ch << std::endl;
-	std::cout << std::dec << "\n" << ch << std::endl;
+// A program with resistant mistakes
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main()
+{
+	string word;
+	cout << "Let's go! Press the return key: ";
+	cin.get();
+	cout << "Enter a word containing three characters at most : ";
+	cin >> setw(3) >> word;
+	cout << "Your input: " << word << endl;
 	return 0;
 }

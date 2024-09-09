2a7a7dbcf900ca06506dda4328bd1fae857df8f2
blossom ghuntla <blossom_ghuntla@yahoo.com>
2024-09-09 22:17:14 +0530

    float operators example

--- cpp_gettingstarted/cpp_gettingstarted.cpp
+++ cpp_gettingstarted/cpp_gettingstarted.cpp
@@ -1,15 +1,12 @@
-// A program with resistant mistakes
-#include <iostream>
-#include <iomanip>
-#include <string>
-using namespace std;
-int main()
-{
-	string word;
-	cout << "Let's go! Press the return key: ";
-	cin.get();
-	cout << "Enter a word containing three characters at most : ";
-	cin >> setw(3) >> word;
-	cout << "Your input: " << word << endl;
+#include<iostream>
+
+int main() {
+	double x;
+	double y;
+
+	std::cout << "\n Enter two floating-point values: ";
+	std::cin >> x >> y;
+	std::cout << "The average of the two numbers are: " << (x + y) / 2.0 << std::endl;
+
 	return 0;
 }

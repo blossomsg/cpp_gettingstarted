f66ff0e6fa277f619531a1478990879b5c5752eb
blossom ghuntla <blossom_ghuntla@yahoo.com>
2026-02-19 23:37:29 +0530

    added utilities header files calling utilities in the main

---
+++ cpp_gettingstarted/utilities.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef UTILITIES
+#include<string>
+
+std::string readFile(const char *filePath);
+void writeFile(const char *filePath, const std::string data);
+
+#endif // !UTILITIES

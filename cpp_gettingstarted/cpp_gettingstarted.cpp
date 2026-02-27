04dc6af524780a867b9c1bb8f5a2df3e314332d0
blossom ghuntla <blossom_ghuntla@yahoo.com>
2026-02-27 22:05:47 +0530

    added ptr login with ->

--- cpp_gettingstarted/cpp_gettingstarted.cpp
+++ cpp_gettingstarted/cpp_gettingstarted.cpp
@@ -1,7 +1,24 @@
+#include "account.h"
 #include "vehicle.h"
+#include <iostream>
+#include <string>
+
+bool getAccount(Account *pAccount);
 
 int main() {
+  Account current1, current2, *ptr = &current1;
   Vehicle bike1, car1;
+
+  ptr->init("John Doe", 123456, 569.00);
+  ptr->display();
+
+  ptr = &current2;
+  if (getAccount(ptr)) {
+	  ptr->display();
+  }
+  else {
+      std::cout << "Invalid input!!!!" << std::endl;
+  }
 
   bike1.init("Apache", "TVS", 160);
   bike1.display();
@@ -10,4 +27,27 @@ int main() {
   car1.display();
 
   return 0;
+}
+
+bool getAccount(Account *pAccount) {
+  std::string name;
+  std::string line(50, '-');
+  unsigned long nr;
+  double startcapital;
+  std::cout << line << "\n"
+            << "Enter data for a new account: \n"
+            << "Account holder: ";
+  if (!getline(std::cin, name) || name.size() == 0) {
+    return false;
+  }
+  std::cout << "Account Number: ";
+  if (!(std::cin >> nr)) {
+    return false;
+  }
+  std::cout << "Starting Capital: ";
+  if (!(std::cin >> startcapital)) {
+    return false;
+  }
+  pAccount->init(name, nr, startcapital);
+  return true;
 }
